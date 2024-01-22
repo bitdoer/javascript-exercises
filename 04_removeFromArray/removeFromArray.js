a3a68a5d8f3feee3a409b45a6d2bd8821c2681f7
bit doer <bitdoer@gmail.com>
2024-01-21 21:43:14 -0600

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removals) {
+    for (let removal of removals) {
+        let idx = array.indexOf(removal);
+        if (idx >= 0) {
+            array.splice(idx, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
