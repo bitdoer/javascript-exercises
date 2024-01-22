05570ca999a0705551aa9e177cd658a14f88829a
bit doer <bitdoer@gmail.com>
2024-01-21 21:50:14 -0600

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof start !== "number" || typeof end !== "number") {
+        return 'ERROR';
+    }
+    let first = (start < end ? start : end);
+    let last = (start > end ? start : end);
+    if (first < 0) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+
+    for (let i = first; i <= last; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
