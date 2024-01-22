77c794a98dd00ac5aefd69b58389cf3f1cc4a44a
bit doer <bitdoer@gmail.com>
2024-01-21 21:59:10 -0600

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    return Math.round((fahrenheit - 32) * 50 / 9) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    return Math.round(celsius * 18 + 320) / 10;
 };
 
 // Do not edit below this line
