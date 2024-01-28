895d0ca156219e7abb1911637eef56b5ed2510c4
bit doer <bitdoer@gmail.com>
2024-01-28 11:45:08 -0600

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+function fibTail(n, last, curr) {
+    switch (parseInt(n, 10)) {
+        case 0:
+            return 0;
+        case 1:
+            return curr;
+        default:
+            if (parseInt(n, 10) < 0) { return "OOPS"; }
+            return fibTail(n - 1, curr, last + curr);
+    }
+}
 
-};
+const fibonacci = (n) => fibTail(n, 0, 1);
 
 // Do not edit below this line
 module.exports = fibonacci;
