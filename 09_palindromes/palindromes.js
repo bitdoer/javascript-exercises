1135e9559061dafefdce3d5b2a8e2e1c9385daec
bit doer <bitdoer@gmail.com>
2024-01-28 11:35:29 -0600

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let chars = [...str.replace(/[^0-9A-Za-z]/g, "")].map((c) => c.toLowerCase());
+    let reversed = chars.slice().reverse();
+    return chars.toString() === reversed.toString();
 };
 
 // Do not edit below this line
