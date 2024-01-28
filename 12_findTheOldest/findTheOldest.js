7196e25d42ba2a183e81f116e80aeea05b252b40
bit doer <bitdoer@gmail.com>
2024-01-28 11:58:25 -0600

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
+const reduction = function(currOld, person) {
+    let oldAge = (currOld.yearOfDeath ?? parseInt(new Date().getFullYear(), 10)) - currOld.yearOfBirth;
+    let personAge = (person.yearOfDeath ?? parseInt(new Date().getFullYear(), 10)) - person.yearOfBirth;
+    return (personAge > oldAge) ? person : currOld;
 };
+
+const findTheOldest = (people) => people.reduce(reduction);
 
 // Do not edit below this line
 module.exports = findTheOldest;
